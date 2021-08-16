09229776731dc26a752351b73330523b86efbdc4
AbdullahArbee <79457888+AbdullahArbee@users.noreply.github.com>
2021-08-16 23:25:21 +0200

    Add files via upload

---
+++ Manipulating Json-Dataset/JSONModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulating_Json_Dataset
+{
+    class JSONModel
+    {
+
+        public class Rootobject
+        {
+            public int id { get; set; }
+            public string model { get; set; }
+            public string colour { get; set; }
+            public string manufacturer { get; set; }
+            public Saleshistory[] salesHistory { get; set; } // Also this is an array due to the nested array in the JSON data so when breaking down we use a loop
+        }
+
+        public class Saleshistory
+        {
+            public int year { get; set; }
+            public int vehiclesSold { get; set; }
+        }
+
+    }
+}
